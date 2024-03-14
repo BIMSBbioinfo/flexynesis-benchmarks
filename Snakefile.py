ab0ca8b0f0e478b5084ef240bee3cc052418be76
Bora Uyar <bora.uyar@mdc-berlin.de>
2024-03-14 12:46:28 +0100

    revert back to downloading data only once; make GNNs obey this structure later

--- Snakefile.py
+++ Snakefile.py
@@ -8,18 +8,17 @@ OUTDIR = os.path.abspath(config['outdir'])
 SRCDIR = os.path.join(os.path.dirname(os.path.abspath(workflow.snakefile)), 'src') 
 LOGDIR = os.path.join(OUTDIR, 'logs')
 RSCRIPT = config['rscript']
+FLEXYNESIS = config['flexynesis'] 
 
-def get_data_url(task_df, prefix):
-    task = task_df[task_df['prefix'] == prefix]['task'].item()
+def get_data_url(task):
     url = config['tasks'][task]['url']
     base = os.path.basename(url)
     name = os.path.splitext(base)[0]
-    #print("url for prefix", prefix, "task:",task)
-    return url, base, name, task
+    return url, base, name
 
 def get_data_path(task_df, prefix):
     task = task_df[task_df['prefix'] == prefix]['task'].item()
-    return os.path.join(OUTDIR, "data", prefix, ".dummy")
+    return os.path.join(OUTDIR, "data", task, ".dummy")
 
 def parse_vars(s):
     # Improved with handling for spaces within the values
@@ -80,9 +79,7 @@ def get_model_args(task_df, prefix):
     
     datapath = os.path.dirname(get_data_path(task_df, prefix))
     
-    args = ["flexynesis --use_gpu"]
-    
-    args.extend([
+    args = [
         "--data_path", datapath,
         "--model_class", task_row['tool'],
         "--fusion_type", task_row['fusion'],
@@ -93,7 +90,7 @@ def get_model_args(task_df, prefix):
         "--use_loss_weighting", task_row['use_loss_weighting'],
         "--data_types", task_row['data_types'],
         "--log_transform", str(task_row['log_transform'])
-    ])
+    ]
     
     if task_row['target']:
         args.extend(["--target_variables", task_row['target']])
@@ -128,11 +125,11 @@ rule all:
         # print analysis table
         os.path.join(OUTDIR, "analysis_table.csv"),
         # input data download
-        expand(os.path.join(OUTDIR, "data", "{analysis}", ".dummy"), analysis = ANALYSES),
+        expand(os.path.join(OUTDIR, "data", "{task}", ".dummy"), task = TASKS),
         # modeling results
         expand(os.path.join(OUTDIR, "results", "{analysis}.{output_type}.csv"), 
                analysis = ANALYSES, 
-               output_type = ['stats', 'feature_importance', 'embeddings_train', 'embeddings_test', 
+               output_type = ['stats', 'baseline.stats', 'feature_importance', 'embeddings_train', 'embeddings_test', 
                              'embeddings_train.filtered', 'embeddings_test.filtered']),
         # dashboard
         os.path.join(OUTDIR, "dashboard.html")
@@ -144,22 +141,24 @@ rule print_analysis_table:
         task_df.to_csv(output[0])
         
 rule download_data:
+    input:
+        os.path.join(OUTDIR, "analysis_table.csv") # make sure analysis table is printed first. 
     output:
-        os.path.join(OUTDIR, "data", "{analysis}", ".dummy") 
+        os.path.join(OUTDIR, "data", "{task}", ".dummy") 
     log: 
-        os.path.join(LOGDIR, "download.{analysis}.log")
+        os.path.join(LOGDIR, "download.{task}.log")
     params: 
-        url = lambda wildcards: get_data_url(task_df, wildcards.analysis)[0],
+        url = lambda wildcards: get_data_url(wildcards.task)[0],
         # download to task folder
-        downloaded_tgz = lambda wildcards: os.path.join(OUTDIR, "data", wildcards.analysis, get_data_url(task_df, wildcards.analysis)[1]),
+        downloaded_tgz = lambda wildcards: os.path.join(OUTDIR, "data", get_data_url(wildcards.task)[1]),
         # extract the folder 
-        extracted =  lambda wildcards: os.path.join(OUTDIR, "data", wildcards.analysis, get_data_url(task_df, wildcards.analysis)[2]),
+        extracted =  lambda wildcards: get_data_url(wildcards.task)[2],
         # name folder to "task" name
-        new_name = lambda wildcards: os.path.join(OUTDIR, "data", wildcards.analysis)
+        new_name = lambda wildcards: os.path.join(OUTDIR, "data", wildcards.task),
     shell:
         """
         curl -L -o {params.downloaded_tgz} {params.url} 
-        tar -xzvf {params.downloaded_tgz} -C {params.new_name}
+        tar -xzvf {params.downloaded_tgz}
         mv {params.extracted}/* {params.new_name}; rm -rf {params.extracted}
         touch {output} 
         """
@@ -169,12 +168,12 @@ rule model:
         lambda wildcards: get_data_path(task_df, wildcards.analysis)
     output: 
         os.path.join(OUTDIR, "results", "{analysis}.stats.csv"),
+        os.path.join(OUTDIR, "results", "{analysis}.baseline.stats.csv"),
         os.path.join(OUTDIR, "results", "{analysis}.feature_importance.csv"),
         os.path.join(OUTDIR, "results", "{analysis}.embeddings_train.csv"),
         os.path.join(OUTDIR, "results", "{analysis}.embeddings_test.csv"),
         os.path.join(OUTDIR, "results", "{analysis}.embeddings_train.filtered.csv"),
         os.path.join(OUTDIR, "results", "{analysis}.embeddings_test.filtered.csv")
-
     log: 
         os.path.join(LOGDIR, "{analysis}.log")
     params: 
@@ -182,7 +181,7 @@ rule model:
         outdir = os.path.join(OUTDIR, "results")
     shell:
         """
-        {params.args} --outdir {params.outdir} --prefix {wildcards.analysis} > {log} 2>&1
+        {FLEXYNESIS} {params.args} --outdir {params.outdir} --prefix {wildcards.analysis} > {log} 2>&1
         """
 
         
